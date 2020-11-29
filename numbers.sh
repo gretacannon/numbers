59878b5dd11316df02383b85a2bdc256b2cf2524
Greta Cannon <grcannon@chapman.edu>
2020-11-29 14:53:30 -0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+# numbers.sh
+# Greta Cannon
+
+echo "Please enter a postitive integer: "
+read NUMBER
+
+N=$1
+
+while [ "$N" -le "$NUMBER" ]
+do
+
+	if [ $((N%2)) -eq 0 ]
+	then
+		echo $N " Even"
+	else
+		echo $N " Odd"
+	fi
+
+	N=$((N+1))
+done
+
